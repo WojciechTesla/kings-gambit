65286beb5a5678ec4a755b841fc0d7851839528c
wbadmin <>
2025-06-09 13:19:45 +0000

    {/src/main/java/com/kings_gambit_151957/project1/Vidkaarls1Combat.java}

--- src/main/java/com/kings_gambit_151957/project1/Vidkaarls1Combat.java
+++ src/main/java/com/kings_gambit_151957/project1/Vidkaarls1Combat.java
@@ -8,21 +8,21 @@ public class Vidkaarls1Combat implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	private java.lang.Boolean win;
+	private java.lang.String outcome;
 
 	public Vidkaarls1Combat() {
 	}
 
-	public java.lang.Boolean getWin() {
-		return this.win;
+	public java.lang.String getOutcome() {
+		return this.outcome;
 	}
 
-	public void setWin(java.lang.Boolean win) {
-		this.win = win;
+	public void setOutcome(java.lang.String outcome) {
+		this.outcome = outcome;
 	}
 
-	public Vidkaarls1Combat(java.lang.Boolean win) {
-		this.win = win;
+	public Vidkaarls1Combat(java.lang.String outcome) {
+		this.outcome = outcome;
 	}
 
 }
